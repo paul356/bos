19f929ce640b37ea4075f7bfca5d820e693f6d54
hao pan <panhao356@gmail.com>
2015-12-07 15:20:51 +0800

    Update calc-angle.clj

--- calc-angle.clj
+++ calc-angle.clj
@@ -1,13 +1,15 @@
 (ns calc-angle.core
-  (:gen-class))
+ (:gen-class))
 
 (def PI 3.1415926)
 
 (def first-arm 14.)
+
 (defn get-first-angle [ang] 
  (+ ang (- 45. 21.36363132)))
 
 (def second-arm 15.2)
+
 (defn get-second-angle [ang] 
  (+ ang (- 60. 41.27344032)))
 
@@ -19,17 +21,26 @@
 
 (defn calc-cos-beta [r z]
  (let [z2 (* z z)
-       r2 (* r r)
-       a (/ 
-          (+ (- (* first-arm first-arm) (* second-arm second-arm)) z2 r2) 
-          (* 2 first-arm))
-       sqrt (Math/sqrt 
-             (- 
-              (+ z2 r2) 
-              (* a a)))]
+  r2 (* r r)
+  a (/ 
+      (+ (- (* first-arm first-arm) (* second-arm second-arm)) z2 r2) 
+      (* 2 first-arm))
+  sqrt (Math/sqrt 
+      (- 
+       (+ z2 r2) 
+       (* a a)))]
   (/ (- (* a r) (* z sqrt)) (+ z2 r2))))
 
+(defn radian-to-degree [ang]
+ (/ (* 180. ang) PI))
+
+(defn calc-alpha-beta [r z]
+ (let [cos-beta (calc-cos-beta r z)
+       sin-beta (Math/sqrt (- 1 (* cos-beta cos-beta)))
+       cos-gamma (/ (- (* first-arm sin-beta) z) second-arm)]
+  (map radian-to-degree (list (- (/ PI 2.) (Math/acos cos-gamma)) (Math/acos cos-beta)))))
+
 (defn -main
-  "main function"
-  [& args]
-  (println "Hello, World!"))
+ "main function"
+ [& args]
+ (print (calc-alpha-beta 10 0)))
