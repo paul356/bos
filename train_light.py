aa7eb12e001412f0747fc555ae3875a497616567
paul <panhao356@gmail.com>
2018-10-26 07:52:33 +0800

    add tflearn simple script

---
+++ train_light.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+TEST_DIR="./train"
+IMG_WIDTH=320
+IMG_HEIGHT=240
+
+def create_data(search_dir):
+    data = []
+    for img in [for fn in os.listdir(search_dir) if fn.endswith(".jpg")]:
+        path = os.path.join(search_dir, img)
+        img_num = img.split('.')[0]
+        img = tf.image.decode_jpeg(path, 3)
+        img = tf.image.resize_image_with_pad(img, IMG_HEIGHT, IMG_WIDTH)
+        testing_data.append([np.array(img), img_num])
+        
+    shuffle(testing_data)
+    np.save('test_data.npy', testing_data)
+    return testing_data
+
+train_data = create_train_data()
+
+tf.reset_default_graph()
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 128, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+if os.path.exists('{}.meta'.format(MODEL_NAME)):
+    model.load(MODEL_NAME)
+    print('model loaded!')
+
+train = train_data[:-500]
+test = train_data[-500:]
+
+X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y = [i[1] for i in train]
+
+test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y = [i[1] for i in test]
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}), 
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
